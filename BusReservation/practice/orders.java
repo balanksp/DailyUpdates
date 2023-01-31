5a74ec396e8b620d644d46776c1b59e4ed5f7a73
balanksp <a.bala101@gmail.com>
2023-01-31 13:47:51 +0530

    ALL CODES

---
+++ BusReservation/practice/orders.java
@@ -0,0 +1,14 @@
+package BusReservation.practice;
+
+public class orders {
+    public static void main(String[] args) {
+        int a=10 , b = 20;
+        int c = (a*10+(b*20));
+        System.out.println(c/100*10);
+
+
+    }
+
+   
+
+}
